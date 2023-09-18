ede12cafd4aff4b2b93c3472a6d0d60ff0d6319f
Evlyn Anker-Baril <evlyn.anker-baril@lcieducation.net>
2023-09-18 19:51:14 -0400

    homework 3.1 needs changes because we dislectict but not me im dine

---
+++ Homework_3/Homework_3_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LoopHomework
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Name : Evlyn
+            //Date: 2023-09-15
+            //Description : Loop Homework
+            {
+                int num1 = 0;
+                while (num1 != 15)
+                {
+                    Console.WriteLine("Enter a number between 1-30");
+                    try
+                    {
+                        num1 = Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.WriteLine("This is an illegal input Enter a number or be gone!");
+                    }
+                    if (num1 < 15 && num1 > 0)
+                    {
+                        Console.WriteLine("Bigger!");
+                    }
+                    else if (num1 > 15 && num1 < 30)
+                    {
+                        Console.WriteLine("Smaller!");
+                    }
+                    else if (num1 > 30 || num1 < 0)
+                    {
+                        Console.WriteLine("The number is out of bounds");
+                    }
+                }
+                Console.WriteLine("You Have Entered the right number!!");
+                Console.ReadLine();
+            }
+        }
+    }
+}
